3147215809ed5c69591198095bd98a1c2a1b415c
seervivarsha24 <seervivarsha24@gmail.com>
2023-02-10 13:44:04 +0530

    Encapsulation completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly r=new RWOnly();
+      //r.name="varsha"; without getter and setter we cannot access private variables or methods
+      r.setName("Varsha");
+      System.out.println(r.getName());
+  }
 }
